4571e3687ae5fdf3e8ec64afe64460c5717f8f0f
Yury_Karpinski <Yury_Karpinski@epam.com>
2016-04-18 17:13:45 +0300

    ok

---
+++ ConsoleApplication1/ClassLib/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Calc
+    {
+        //public int a, b;
+
+        public int Summ (int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply (int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Difference (int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Division (int a, int b)
+        {
+            return (double) a / b;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 10, b = 2;
+            Calc calculator = new Calc();
+            Assert.AreEqual(calculator.Summ(a, b), 12);
+            Assert.AreEqual(calculator.Difference(a, b), 8);
+            Assert.AreEqual(calculator.Multiply(a, b), 20);
+            Assert.AreEqual(calculator.Division(a, b), 5.0);
+        }
+    }
+}
