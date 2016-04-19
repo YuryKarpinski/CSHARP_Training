78469e612a4765031409c5e8d03cdfc759c50fed
Yury_Karpinski <Yury_Karpinski@epam.com>
2016-04-19 17:21:15 +0300

    file added

---
+++ ConsoleApplication1/ConsoleApplication_Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            //All chars to lower case
+            s = s.ToLower();
+            
+            //Current time
+            DateTime d = DateTime.Now;
+            
+            //Definig sentences, adding date to the begining
+            s = s.Replace(". ", $".\n{d: dd:mm:yyyy HH:ss:msms} ");
+            s = s.Replace("! ", $"!\n{d: dd:mm:yyyy HH:ss:msms} ");
+            s = s.Replace("? ", $"?\n{d: dd:mm:yyyy HH:ss:msms} ");
+            
+            // Adding date to the first sentence
+            s = $"{d : dd:mm:yyyy HH:ss:msms} " + s;
+            
+            //printing result
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
